d139a93a6cea997b1fa2aa288a3130f652f2d814
Andrew Laptev <laptewandrew@gmail.com>
2024-04-04 17:31:23 +0300

    beta

---
+++ js/main.js
@@ -0,0 +1,42 @@
+const field = document.querySelector(".field__wrapper");
+
+// generate random background color
+function randomColor() {
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(255);
+        return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
+    }
+
+    let firstNumber = Number(getRandomIntInclusive());
+    let secondNumber = Number(getRandomIntInclusive());
+    let thirdNumber = Number(getRandomIntInclusive());
+    return `rgb(${firstNumber},${secondNumber},${thirdNumber})`;
+}
+
+function createBlocks(number) {
+    for (let index = 0; index < number; index++) {
+        const row = document.createElement("div");
+        row.classList.add("row")
+        field.appendChild(row);
+
+        for (let index = 0; index < number; index++) {
+            const column = document.createElement("div");
+            column.classList.add("column");
+            row.appendChild(column);
+
+        }
+    }
+}
+createBlocks(100);
+
+function paintBlock() {
+    const columns = document.querySelectorAll(".column");
+
+    columns.forEach(element => {
+        element.addEventListener("mouseenter", function () {
+            element.style.background = randomColor();
+        })
+    });
+}
+paintBlock();

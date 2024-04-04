22db14c6597c1171afa164b39d4576fbaec5a759
Andrew Laptev <laptewandrew@gmail.com>
2024-04-04 23:35:34 +0300

    rewrite drawing function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ const field = document.querySelector(".field__wrapper");
 const eraseBtn = document.querySelector(".erase-btn");
 const inputElement = document.querySelector(".field__input");
 
+function drawing(){
+    const columns = document.querySelectorAll(".column");
+    columns.forEach(element => {
+        element.addEventListener("mousemove", function(event){
+            if(event.buttons === 1){
+                element.style.background = "black";
+            }
+            // отключаем выделение и перетаскивание
+            element.style.userSelect = "none";
+        })
+    });
+}
+
 // Функция для создания блоков
 function createBlocks(number) {
     // Если число не указано, используем значение по умолчанию 16
@@ -20,6 +33,7 @@ function createBlocks(number) {
             row.appendChild(column);
         }
     }
+    drawing();
 }
 
 // Добавляем обработчик событий для поля ввода
@@ -47,65 +61,6 @@ createBlocks(16);
 window.onload = function() {
     inputElement.value = '';
 };
-
-// // generate random background color
-// function randomColor() {
-//     function getRandomIntInclusive(min, max) {
-//         min = Math.ceil(0);
-//         max = Math.floor(255);
-//         return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
-//     }
-
-//     let firstNumber = Number(getRandomIntInclusive());
-//     let secondNumber = Number(getRandomIntInclusive());
-//     let thirdNumber = Number(getRandomIntInclusive());
-//     return `rgb(${firstNumber},${secondNumber},${thirdNumber})`;
-// }
-
-// function paintBlock() {
-//     const columns = document.querySelectorAll(".column");
-//     const randomColorBtn = document.querySelector(".random-color-btn"); // предположим, что у вас есть кнопка с id="randomColorBtn"
-//     let isDrawing = false;
-//     let randomBtnClick = false;
-
-//     // проверка нажата ли левая клавиша мыши
-//     document.addEventListener('mousedown', function(event) {
-//         if (event.button == 0) { 
-//             isDrawing = true;
-//         }
-//     });
-
-//     document.addEventListener('mouseup', function(event) {
-//         if (event.button == 0) { 
-//             isDrawing = false;
-//         }
-//     });
-
-//     // Проверка нажата ли кнопка random color
-//     randomColorBtn.addEventListener("click", function(event){
-//         randomBtnClick = !randomBtnClick;
-//     })
-    
-//     columns.forEach(element => {
-//         element.addEventListener("mousemove", function () {
-//             if (isDrawing) {
-//                 if(randomBtnClick){
-//                     element.style.background = randomColor();
-//                 } else {
-//                     element.style.background = "black";
-//                 }
-//             }
-//         })
-//     });
-
-//     columns.forEach(element => {
-//         eraseBtn.addEventListener("click", function(){
-//             element.style.background = "rgb(255,255,255)";
-//         })
-//     })
-// }
-
-// paintBlock()
 
 
 
